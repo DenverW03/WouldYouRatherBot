a2d99576062f4a6000f073ba479e4e8e768c6558
DenverW03 <denverwhiteley@hotmail.com>
2024-09-11 22:36:30 +0100

    main function now exposes the rest api

--- rest_api/src/main.rs
+++ rest_api/src/main.rs
@@ -1,5 +1,16 @@
+use actix_web::{App, HttpServer};
+
 mod rest_api;
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Exposing the REST API on the IP and port specified, just locall whilst in current early dev
+    // stage
+    HttpServer::new(|| {
+        App::new()
+            .service(rest_api::generate)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
