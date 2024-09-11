cd791a3747195a50e88a6c644bfe7baef9c94842
DenverW03 <denverwhiteley@hotmail.com>
2024-09-11 21:57:09 +0100

    restructured to prep for extension into full SAAS

--- video_tool/main.py
+++ video_tool/main.py
@@ -1,6 +1,6 @@
 from generate_video import video_builder
 
-
+# Main function is used to drive the steps of building a video
 if __name__ == '__main__':
     vid_builder = video_builder("Be a chef", "Be a doctor", "resources/upper.jpg", "resources/lower.jpg")
     vid_builder.build()
