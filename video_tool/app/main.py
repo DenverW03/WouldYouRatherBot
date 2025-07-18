c6581f920054371c43759728f0ae29930ee6209f
DenverW03 <denverwhiteley@hotmail.com>
2025-07-18 16:39:15 +0100

    refactoring some things

---
+++ video_tool/app/main.py
@@ -0,0 +1,5 @@
+from app.generate_video import build_video
+
+# Main function is used to drive the steps of building a video
+if __name__ == '__main__':
+    build_video("Be a chef", "Be a doctor", "../resources/upper.jpg", "../resources/lower.jpg", "../out/video.mp4")
